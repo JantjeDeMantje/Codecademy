0fa14578978ff074f0d01ac0dbc170e22434d3d0
JanRoelofs <janroe2004@gmail.com>
2024-08-02 13:49:45 +0200

    StudentScreenController toegevoegd
    StudentScreenController met de volgende methodes:
    
    - initialize om tabel te vullen en listener toe te voegen
    
    - fillTable om date op te halen te toe te voegen aan tabel
    
    - handleBackButton voor de backButton
    
    - tableSelectionListener om selecties van de tabel op te vangen
    
    - handleRowSelect om de data van een geselecteerde student te laten zien in textFields
    
    - handleCreateButton om data in TextFields in tabel te zetten
    
    - handleDeleteButton om geselecteerde student te verwijderen
    
    - handleUpdateButton om geselecteerde student te updaten met data uit textfields

---
+++ codecademy-sietse-jan/src/main/java/com/codecademy/controllers/StudentScreenController.java
@@ -0,0 +1,159 @@
+package com.codecademy.controllers;
+
+import java.util.Date;
+
+import com.codecademy.GUI;
+import com.codecademy.domain.Course;
+import com.codecademy.domain.Student;
+import com.codecademy.logic.StudentManager;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+public class StudentScreenController {
+
+    private StudentManager studentManager;
+
+    @FXML
+    private TableView<Student> studentTable;
+
+    @FXML
+    private TableColumn<Student, String> emailColumn;
+
+    @FXML
+    private TableColumn<Student, Date> birthdateColumn;
+
+    @FXML
+    private TableColumn<Student, String> nameColumn;
+
+    @FXML
+    private Button createBTN;
+
+    @FXML
+    private Button deleteBTN;
+
+    @FXML
+    private Button registerBTN;
+
+    @FXML
+    private Button updateBTN;
+
+    @FXML
+    private Button backBTN;
+
+    @FXML
+    private TextField nameTEXT;
+
+    @FXML
+    private TextField postcodeTEXT;
+
+    @FXML
+    private TextField emailTEXT;
+
+    @FXML
+    private TextField genderTEXT;
+
+    @FXML
+    private TextField birthdateTEXT;
+
+    @FXML
+    private TextField cityTEXT;
+
+    @FXML
+    private TextField countryTEXT;
+
+    public void initialize() {
+        studentManager = new StudentManager();
+        fillTable();
+        tableSelectionListener();
+    }
+
+    @FXML
+    private void fillTable() { // This method fills the table with the data from the courseManager.
+
+        studentManager.getStudents().forEach(student -> { // Adds all the data from the arraylist to the table.
+            studentTable.getItems().add(student);
+        });
+
+        // Set cell value factories for each column
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        emailColumn.setCellValueFactory(new PropertyValueFactory<>("email"));
+        birthdateColumn.setCellValueFactory(new PropertyValueFactory<>("birthdate"));
+    }
+
+    @FXML
+    private void handleBackButton(ActionEvent event) { // This method handles the back button.
+        System.out.println("Back button clicked"); // Logs the activation of the back button.
+        GUI.instance.setRoot("homeScreen.fxml"); // Loads the new fxml in.
+    }
+
+    private void tableSelectionListener() { // This method looks if a row is selected.
+        studentTable.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                handleRowSelect(newValue);
+            }
+        });
+    }
+
+    private void handleRowSelect(Student selectedStudent) { // This method handels the selected row.
+        System.out.println("Selected student: " + selectedStudent.getName()); // Logs the selected courseName
+
+        nameTEXT.setText(selectedStudent.getName());
+        emailTEXT.setText(selectedStudent.getEmail());
+        birthdateTEXT.setText(selectedStudent.getBirthdate());
+        genderTEXT.setText(selectedStudent.getGender());
+        postcodeTEXT.setText(selectedStudent.getPostcode());
+        cityTEXT.setText(selectedStudent.getCity());
+        countryTEXT.setText(selectedStudent.getCountry());
+    }
+
+    @FXML
+    void handleCreateButton(ActionEvent event) {
+        String studentName = nameTEXT.getText();
+        String studentEmail = emailTEXT.getText();
+        String studentBirthdate = birthdateTEXT.getText();
+        String studentGender = genderTEXT.getText();
+        String studentPostcode = postcodeTEXT.getText();
+        String studentCity = cityTEXT.getText();
+        String studentCountry = countryTEXT.getText();
+
+        Student newStudent = new Student(studentName, studentEmail, studentBirthdate, studentGender, studentPostcode, studentCity, studentCountry);
+        System.out.println(newStudent.getName());
+
+        studentTable.getItems().add(newStudent);
+
+    }
+
+    @FXML
+    void handleDeleteButton(ActionEvent event) {
+        Student selectedStudent = studentTable.getSelectionModel().getSelectedItem();
+        studentTable.getItems().remove(selectedStudent);
+
+    }
+
+    @FXML
+    void handleRegisterButton(ActionEvent event) {
+        //TODO: Start RegisterScreen with selected student
+
+    }
+
+    @FXML
+    void handleUpdateButton(ActionEvent event) {
+        Student selectedStudent = studentTable.getSelectionModel().getSelectedItem();
+        selectedStudent.setName(nameTEXT.getText());
+        selectedStudent.setEmail(emailTEXT.getText());
+        selectedStudent.setBirthdate(birthdateTEXT.getText());
+        selectedStudent.setGender(genderTEXT.getText());
+        selectedStudent.setPostcode(postcodeTEXT.getText());
+        selectedStudent.setCity(cityTEXT.getText());
+        selectedStudent.setCountry(countryTEXT.getText());
+        studentTable.refresh();
+
+    }
+
+}
