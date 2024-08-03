b7e4f59e766256be2edd120c12767d622a073356
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2024-08-03 20:51:20 +0200

    SelectedStudent dataholder
    + Added the selected student in the dataholder.

--- codecademy-sietse-jan/src/main/java/com/codecademy/dataStorage/DataHolder.java
+++ codecademy-sietse-jan/src/main/java/com/codecademy/dataStorage/DataHolder.java
@@ -1,4 +1,21 @@
 package com.codecademy.dataStorage;
 
+import com.codecademy.domain.Student;
+
 public class DataHolder {
+    private Student selectedStudent;
+
+    private static final DataHolder holder = new DataHolder();
+
+    public static DataHolder getInstance() {
+        return holder;
+    } 
+
+    public void setSelectedStudent(Student selectedStudent){
+        this.selectedStudent = selectedStudent;
+    }
+ 
+    public Student getSelectedStudent(){
+        return selectedStudent;
+    }
 }
