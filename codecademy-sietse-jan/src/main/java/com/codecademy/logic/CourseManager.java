aaa2767d2b31afb150d8c2c839286b97b7e7ce85
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2024-08-03 22:33:56 +0200

    Temp. dummy data update
    +/- changed dummy data

--- codecademy-sietse-jan/src/main/java/com/codecademy/logic/CourseManager.java
+++ codecademy-sietse-jan/src/main/java/com/codecademy/logic/CourseManager.java
@@ -15,11 +15,11 @@ public class CourseManager {
     private void createDummyData() { // This method creates the temp. dummy data
         courses = new ArrayList<Course>();
 
-        courses.add(new Course("Webdesign", "HTML uitleg", "Wauw wat is HTML toch lastig, maar of nee toch niet", Difficulty.Beginner));
-        courses.add(new Course("Java", "JavaFX gebruik", "Java is al lastig, maar nu kan je ook met JavaFX iets maken!", Difficulty.Gevorderd));
-        courses.add(new Course("Webdesign", "JavaScript gebruik", "Naast HTML en CSS, kan je ook gebruik maken van JavaScript!", Difficulty.Expert));
-        courses.add(new Course("Database", "SQL manager", "Wat zijn queries en hoe maak je ze? Leer dat in deze cursus!", Difficulty.Beginner));
-        courses.add(new Course("Binair", "Binair puzzelen", "Is sudoku te makkelijk voor jou? Probeer dan nu de nieuwe puzzel Binair!", Difficulty.Gevorderd));
+        courses.add(new Course("Webdesign 1", "HTML uitleg", "Wauw wat is HTML toch lastig, maar of nee toch niet", Difficulty.Beginner));
+        courses.add(new Course("Java 1", "JavaFX gebruik", "Java is al lastig, maar nu kan je ook met JavaFX iets maken!", Difficulty.Gevorderd));
+        courses.add(new Course("Webdesign 2", "JavaScript gebruik", "Naast HTML en CSS, kan je ook gebruik maken van JavaScript!", Difficulty.Expert));
+        courses.add(new Course("Database 1", "SQL manager", "Wat zijn queries en hoe maak je ze? Leer dat in deze cursus!", Difficulty.Beginner));
+        courses.add(new Course("Binair 1", "Binair puzzelen", "Is sudoku te makkelijk voor jou? Probeer dan nu de nieuwe puzzel Binair!", Difficulty.Gevorderd));
     }
 
     public ArrayList<Course> getCourses() {
