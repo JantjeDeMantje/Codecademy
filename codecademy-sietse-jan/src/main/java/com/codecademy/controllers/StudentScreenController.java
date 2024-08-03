01dede7b04fa8444e13ecac527bd967942555890
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2024-08-03 17:32:26 +0200

    Squashed commit of the following:
    commit e8eea18d36e5e87426f63c5b8c21aa11ae574213
    Author: Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
    Date:   Sat Aug 3 17:29:31 2024 +0200
    
        UpdateStudent update
    
        +/- changed responsibility from the controller to the manager
    
        + created updateStudent method in the manager
    
    commit 43abf0d306fb21352fb208ed2f770aa357a28f43
    Author: Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
    Date:   Sat Aug 3 17:28:24 2024 +0200
    
        DeleteStudent update
    
        +/- changed responsibility from the controller to the manager
    
        + Added deleteStudent method in the manager
    
    commit ef45c6c547fb0f9a0eb19bca0749308ab19337f9
    Author: Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
    Date:   Sat Aug 3 17:27:47 2024 +0200
    
        CreateStudent update
    
        +/- changed responsibility from the controller to the manager
    
    commit a52e23adb7d15d39f1bfb2e17a75bd4188b3ce96
    Author: Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
    Date:   Sat Aug 3 17:07:49 2024 +0200
    
        Commentary update
    
        + Added comments to all methods
    
    commit 86b2f309efaaedd8c9eef2b5a99b171381f27f1e
    Author: Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
    Date:   Sat Aug 3 17:04:35 2024 +0200
    
        Duplicate code fix
    
        + Added private Student selectedStudent
    
        - Removed unnessencary code in:
        handleDeleteButton
        handleUpdateButton
    
    commit 122160a0b9e5b434044e81956dab85a3885ecdb7
    Author: Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
    Date:   Sat Aug 3 16:59:41 2024 +0200
    
        Removing unnessencary code
    
        - Removed unnessencary import
    
    commit 65344080201f24973c3d334e0c5f999f84137bc3
    Author: Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
    Date:   Sat Aug 3 16:58:29 2024 +0200
    
        Creation of createStudent method
    
        + Created createStudent method
        + functionality code: It creates the student.
    
        -/+ changed temp.dummy data
    
    commit 77d600354e5482a79ee488ec242a488dcb138e68
    Author: Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
    Date:   Sat Aug 3 16:56:46 2024 +0200
    
        Reordering update
    
        -/+ reorderd the backButtonHandler
    
    commit 23ae5003a10bc4216abc907dff9b84cd02b60dd5
    Author: Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
    Date:   Sat Aug 3 16:55:21 2024 +0200
    
        Create manager update
    
        Changed createbutton functionality:
        - Removed all student strings
        + added createStudent call from the studentManager
    
    commit 0fa14578978ff074f0d01ac0dbc170e22434d3d0
    Author: JanRoelofs <janroe2004@gmail.com>
    Date:   Fri Aug 2 13:49:45 2024 +0200
    
        StudentScreenController toegevoegd
    
        StudentScreenController met de volgende methodes:
    
        - initialize om tabel te vullen en listener toe te voegen
    
        - fillTable om date op te halen te toe te voegen aan tabel
    
        - handleBackButton voor de backButton
    
        - tableSelectionListener om selecties van de tabel op te vangen
    
        - handleRowSelect om de data van een geselecteerde student te laten zien in textFields
    
        - handleCreateButton om data in TextFields in tabel te zetten
    
        - handleDeleteButton om geselecteerde student te verwijderen
    
        - handleUpdateButton om geselecteerde student te updaten met data uit textfields
    
    commit 18862dccbe3129b62e4a86fba177eb264c66cf24
    Author: JanRoelofs <janroe2004@gmail.com>
    Date:   Fri Aug 2 13:37:46 2024 +0200
    
        StudentManager toegevoegd
    
        Student classe aangemaakt met bijbehorende data
        StudentManager aangemaakt met dummy studenten
    
    commit c4ad6af67076197eda5205e37ca46d52dec284b9
    Author: JanRoelofs <janroe2004@gmail.com>
    Date:   Fri Aug 2 13:36:58 2024 +0200
    
        student scherm toegevoegd
    
        Student scherm heeft:
    
        Tabel met info over studenten
        Lijst met data van geselecteerde student
        Knop voor het maken van een student
        Knop voor het verwijderen van een student
        Knop voor het updaten van een student
        Knop om een student in te schrijven bij een course

---
+++ codecademy-sietse-jan/src/main/java/com/codecademy/controllers/StudentScreenController.java
@@ -0,0 +1,156 @@
+package com.codecademy.controllers;
+
+import java.util.Date;
+
+import com.codecademy.GUI;
+import com.codecademy.domain.Student;
+import com.codecademy.logic.StudentManager;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+public class StudentScreenController {
+
+    private StudentManager studentManager;
+
+    private Student selectedStudent;
+
+    @FXML
+    private TableView<Student> studentTable;
+
+    @FXML
+    private TableColumn<Student, String> emailColumn;
+
+    @FXML
+    private TableColumn<Student, Date> birthdateColumn;
+
+    @FXML
+    private TableColumn<Student, String> nameColumn;
+
+    @FXML
+    private Button createBTN;
+
+    @FXML
+    private Button deleteBTN;
+
+    @FXML
+    private Button registerBTN;
+
+    @FXML
+    private Button updateBTN;
+
+    @FXML
+    private Button backBTN;
+
+    @FXML
+    private TextField nameTEXT;
+
+    @FXML
+    private TextField postcodeTEXT;
+
+    @FXML
+    private TextField emailTEXT;
+
+    @FXML
+    private TextField genderTEXT;
+
+    @FXML
+    private TextField birthdateTEXT;
+
+    @FXML
+    private TextField cityTEXT;
+
+    @FXML
+    private TextField countryTEXT;
+
+    public void initialize() {
+        studentManager = new StudentManager();
+        fillTable();
+        tableSelectionListener();
+    }
+
+    @FXML
+    private void fillTable() { // This method fills the table with the data from the courseManager.
+
+        studentManager.getStudents().forEach(student -> { // Adds all the data from the arraylist to the table.
+            studentTable.getItems().add(student);
+        });
+
+        // Set cell value factories for each column
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        emailColumn.setCellValueFactory(new PropertyValueFactory<>("email"));
+        birthdateColumn.setCellValueFactory(new PropertyValueFactory<>("birthdate"));
+    }
+
+    private void tableSelectionListener() { // This method looks if a row is selected.
+        studentTable.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                handleRowSelect(newValue);
+            }
+        });
+    }
+
+    private void handleRowSelect(Student selectedStudent) { // This method handels the selected row.
+        System.out.println("Selected student: " + selectedStudent.getName()); // Logs the selected courseName
+
+        this.selectedStudent = selectedStudent; // Sets the selected student to the selected student in the table.
+
+        // Sets the textfields to the data of the selected student.
+        nameTEXT.setText(selectedStudent.getName());
+        emailTEXT.setText(selectedStudent.getEmail());
+        birthdateTEXT.setText(selectedStudent.getBirthdate());
+        genderTEXT.setText(selectedStudent.getGender());
+        postcodeTEXT.setText(selectedStudent.getPostcode());
+        cityTEXT.setText(selectedStudent.getCity());
+        countryTEXT.setText(selectedStudent.getCountry());
+    }
+
+    @FXML
+    private void handleBackButton(ActionEvent event) { // This method handles the back button.
+        System.out.println("Back button clicked"); // Logs the activation of the back button.
+        GUI.instance.setRoot("homeScreen.fxml"); // Loads the new fxml in.
+    }
+
+    @FXML
+    void handleCreateButton(ActionEvent event) { // This method handles the create button.
+
+        Student newStudent = studentManager.createStudent(
+                nameTEXT.getText(), emailTEXT.getText(),
+                birthdateTEXT.getText(), genderTEXT.getText(),
+                postcodeTEXT.getText(), cityTEXT.getText(),
+                countryTEXT.getText(), studentTable);
+
+        System.out.println("CreateStudentButton pressed: " + newStudent.getName()); // Logs the name of the new student
+    }
+
+    @FXML
+    void handleDeleteButton(ActionEvent event) { // This method handles the delete button.
+        studentManager.deleteStudent(selectedStudent, studentTable); // Removes the selected student from the table.
+
+        // Logs the name of the deleted student
+        System.out.println("DeleteStudentButton pressed: " + selectedStudent.getName());
+    }
+
+    @FXML
+    void handleRegisterButton(ActionEvent event) { // This method handles the register button.
+
+    }
+
+    @FXML
+    void handleUpdateButton(ActionEvent event) { // This method handles the update button.
+        studentManager.updateStudent(
+                selectedStudent, nameTEXT.getText(), emailTEXT.getText(),
+                birthdateTEXT.getText(), genderTEXT.getText(),
+                postcodeTEXT.getText(), cityTEXT.getText(),
+                countryTEXT.getText(), studentTable);
+
+        // Logs the name of the updated student
+        System.out.println("UpdateStudentButton pressed: " + selectedStudent.getName());
+    }
+
+}
---
+++ codecademy-sietse-jan/src/main/java/com/codecademy/domain/Student.java
@@ -0,0 +1,81 @@
+package com.codecademy.domain;
+
+public class Student {
+
+    private String name;
+    private String email;
+    private String birthdate;
+    private String gender;
+    private String postcode;
+    private String city;
+    private String country;
+
+    public Student(String name, String email, String birthdate, String gender, String postcode, String city, String country) {
+        this.name = name;
+        this.email = email;
+        this.birthdate = birthdate;
+        this.gender = gender;
+        this.postcode = postcode;
+        this.city = city;
+        this.country = country;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    
+    
+}
