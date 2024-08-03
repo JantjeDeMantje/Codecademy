d080aceecde5d737f5751f3a281bbad8bc88536d
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2024-08-04 00:16:05 +0200

    RegistrationManager update
    + Added student in the constructor (for use in the dummy data)

--- codecademy-sietse-jan/src/main/java/com/codecademy/logic/RegistrationManager.java
+++ codecademy-sietse-jan/src/main/java/com/codecademy/logic/RegistrationManager.java
@@ -16,15 +16,15 @@ public class RegistrationManager {
     private ArrayList<Registration> registrations = new ArrayList<>();
     private ArrayList<Course> courses = new ArrayList<>();
     
-    public RegistrationManager() {
+    public RegistrationManager(Student student) {
         loadCourseManager();
-        createDummyData();
+        createDummyData(student);
     }
 
-    private void createDummyData() { // This method creates the temp. dummy data
-        registrations.add(new Registration(DataHolder.getInstance().getSelectedStudent(), courses.get(1), "20-03-2024"));
-        registrations.add(new Registration(DataHolder.getInstance().getSelectedStudent(), courses.get(3), "24-05-2024"));
-        registrations.add(new Registration(DataHolder.getInstance().getSelectedStudent(), courses.get(4), "01-08-2024"));
+    private void createDummyData(Student student) { // This method creates the temp. dummy data
+        registrations.add(new Registration(student, courses.get(1), "20-03-2024"));
+        registrations.add(new Registration(student, courses.get(3), "24-05-2024"));
+        registrations.add(new Registration(student, courses.get(4), "01-08-2024"));
     }
 
     private void loadCourseManager(){ // This method loads all information needed from the courseManager.
