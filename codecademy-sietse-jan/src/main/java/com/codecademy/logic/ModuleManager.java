5529c5fbae65cf165da09d8be0e2177e6a88dc42
JanRoelofs <janroe2004@gmail.com>
2024-08-24 16:05:50 +0200

    ModuleManager toegevoegd voor tijdelijke dummyData

---
+++ codecademy-sietse-jan/src/main/java/com/codecademy/logic/ModuleManager.java
@@ -0,0 +1,29 @@
+package com.codecademy.logic;
+
+import java.util.ArrayList;
+import com.codecademy.domain.Module;
+
+public class ModuleManager {
+
+    public ArrayList<Module> modules;
+
+    public ModuleManager() {
+        createDummyData();
+    }
+
+    private void createDummyData() {
+        modules = new ArrayList<>();
+        modules.add(new Module("Java 1", "1.0", "This module is about Java 1", "Jan", "jan@mail.com", 1));
+        modules.add(new Module("Java 2", "1.0", "This module is about Java 2", "Jan", "jan@mail.com", 2));
+        modules.add(new Module("Java 3", "1.0", "This module is about Java 3", "Jan", "jan@mail.com", 3));
+        modules.add(new Module("Java 4", "1.0", "This module is about Java 4", "Jan", "jan@mail.com", 4));
+        modules.add(new Module("Java 5", "1.0", "This module is about Java 5", "Jan", "jan@mail.com", 5));
+    }
+
+    public ArrayList<Module> getModules() {
+        return modules;
+    }
+
+
+    
+}
