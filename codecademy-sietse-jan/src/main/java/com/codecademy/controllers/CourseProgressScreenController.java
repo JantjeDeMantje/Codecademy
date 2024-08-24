70f12045339528feee57e9802fb23cfb5806d35a
JanRoelofs <janroe2004@gmail.com>
2024-08-24 16:09:18 +0200

    Tabellen op CourseProgressScreen worden nu soort van ingevuld
    - Student Tabel wordt ingevuld met studenten en test voortgang data
    
    - Module tabel wordt ingevult met modules, zonder voortgang

--- codecademy-sietse-jan/src/main/java/com/codecademy/controllers/CourseProgressScreenController.java
+++ codecademy-sietse-jan/src/main/java/com/codecademy/controllers/CourseProgressScreenController.java
@@ -1,17 +1,28 @@
 package com.codecademy.controllers;
 
 import com.codecademy.GUI;
+import com.codecademy.dataStorage.DataHolder;
+import com.codecademy.domain.Course;
+import com.codecademy.domain.WatchPercentage;
+import com.codecademy.logic.CourseProgressManager;
+import com.codecademy.domain.Module;
 
+import javafx.beans.property.SimpleStringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 
 public class CourseProgressScreenController {
+
+    private Course selectedCourse;
+
+    private CourseProgressManager courseProgressManager;
 
     @FXML
     private Label achievedCountLAB;
@@ -38,28 +49,68 @@ public class CourseProgressScreenController {
     private TextFlow introtextTV;
 
     @FXML
-    private TableView<?> moduleProgressTable;
+    private TableView<Module> moduleProgressTable;
 
     @FXML
-    private TableColumn<?, ?> modulesColumn;
+    private TableColumn<Module, Integer> modulesColumn;
 
     @FXML
-    private TableColumn<?, ?> progressColumn1;
+    private TableColumn<WatchPercentage, Integer> progressColumn1;
 
     @FXML
-    private TableColumn<?, ?> progressColumn2;
+    private TableColumn<Module, Integer> progressColumn2;
 
     @FXML
-    private TableColumn<?, ?> studentsColumn;
+    private TableColumn<WatchPercentage, String> studentsColumn;
 
     @FXML
-    private TableView<?> studentsProgressTable;
+    private TableView<WatchPercentage> studentsProgressTable;
 
     @FXML
     private Label subjectLAB;
 
     @FXML
     private Text subjectTV;
+
+    public void initialize() { // This method initializes the course progress screen.
+        getinformation();
+        courseProgressManager = new CourseProgressManager(selectedCourse); // Creates a new courseProgressManager object.
+        setLabels();
+        fillStudentsTable();
+        fillModulesTable();
+    }
+
+    private void getinformation() { // This method gets all information from the dataholder. (last screen)
+        selectedCourse = DataHolder.getInstance().getSelectedCourse();
+    }
+
+    private void setLabels() { // This method sets all the labels with the information from the selected
+                               // course.
+        courseNameLAB.setText(selectedCourse.getCourseName());
+        difficultyTV.setText(selectedCourse.getDifficulty().toString());
+        subjectTV.setText(selectedCourse.getSubject());
+        introtextTV.getChildren().add(new Text(selectedCourse.getIntroductionText()));
+        achievedCountTV.setText(String.valueOf(10 + (int) ((100 - 10 + 10) * Math.random())));
+    }
+
+    private void fillStudentsTable() { // This method fills the table with the data from the courseProgressManager.
+        courseProgressManager.getWatchPercentages().forEach(watchPercentage -> { // Adds all the data from the arraylist to the table.
+            studentsProgressTable.getItems().add(watchPercentage);
+
+        });
+
+        studentsColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getStudent().getName()));
+        progressColumn1.setCellValueFactory(new PropertyValueFactory<>("watchPercentage"));
+    }
+
+    private void fillModulesTable() { // This method fills the table with the data from the courseProgressManager.
+        courseProgressManager.getModules().forEach(module -> { // Adds all the data from the arraylist to the table.
+            moduleProgressTable.getItems().add(module);
+        });
+
+        modulesColumn.setCellValueFactory(new PropertyValueFactory<>("title"));
+    }
+
 
     @FXML
     void handleBackButton(ActionEvent event) {// This method handles the back button.
