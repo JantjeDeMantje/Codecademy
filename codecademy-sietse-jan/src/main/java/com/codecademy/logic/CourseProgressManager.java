eda16985385533db9ddc0d7109f6d51504b13ba7
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2024-09-03 12:00:47 +0200

    Update CourseProgressManager
    + Added all new stuff from the watchPercentageManager

--- codecademy-sietse-jan/src/main/java/com/codecademy/logic/CourseProgressManager.java
+++ codecademy-sietse-jan/src/main/java/com/codecademy/logic/CourseProgressManager.java
@@ -1,9 +1,11 @@
 package com.codecademy.logic;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 import com.codecademy.domain.Course;
 import com.codecademy.domain.Module;
+import com.codecademy.domain.Student;
 import com.codecademy.domain.WatchPercentage;
 
 
@@ -14,20 +16,22 @@ public class CourseProgressManager {
 
     private ArrayList<Module> modules;
     private ArrayList<WatchPercentage> watchPercentages;
+    private Map<Student, Double> averageWatchPercentages;
 
     public CourseProgressManager(Course course) {
         loadModuleManager();
         loadWatchProgressManager();
     }
 
-    private void loadModuleManager() {
+    private void loadModuleManager() { // Loads all modules
         moduleManager = new ModuleManager();
         this.modules = moduleManager.getModules();
     }
 
-    private void loadWatchProgressManager() {
+    private void loadWatchProgressManager() { // Loads all watchProgresses
         watchProgressManager = new WatchProgressManager();
         this.watchPercentages = watchProgressManager.getWatchPercentages();
+        this.averageWatchPercentages = watchProgressManager.getAverageWatchPercentagePerStudent();
     }
 
     public ArrayList<Module> getModules() {
@@ -38,10 +42,7 @@ public class CourseProgressManager {
         return watchPercentages;
     }
 
-    
-
-
-
-
-    
+    public Map<Student, Double> getAverageWatchPercentages() {
+        return averageWatchPercentages;
+    }    
 }
