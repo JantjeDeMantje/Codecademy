f7edbdb1c67864c10a62ec217f53c5360911c7eb
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2024-09-03 11:59:34 +0200

    Update Dummydata
    + Created a new dummy data method

--- codecademy-sietse-jan/src/main/java/com/codecademy/logic/WatchProgressManager.java
+++ codecademy-sietse-jan/src/main/java/com/codecademy/logic/WatchProgressManager.java
@@ -2,9 +2,9 @@ package com.codecademy.logic;
 
 import java.util.ArrayList;
 
-import com.codecademy.domain.Course;
 import com.codecademy.domain.Student;
 import com.codecademy.domain.WatchPercentage;
+import com.codecademy.domain.Module;
 
 public class WatchProgressManager {
 
@@ -22,20 +22,22 @@ public class WatchProgressManager {
         getAverageWatchPercentagePerStudent();
     }
 
-    private void createDummyData() {
-        watchPercentages.add(new WatchPercentage(students.get(0), courses.get(0), 50.0));
-        watchPercentages.add(new WatchPercentage(students.get(1), courses.get(1), 75.0));
-        watchPercentages.add(new WatchPercentage(students.get(2), courses.get(2), 0.0));
+    private void createDummyData() { // Creates temp. dummy data for testing purposes.
+        for (Student student : students) {
+            for (Module module : modules) {
+                watchPercentages.add(new WatchPercentage(student, module, Math.round(Math.random() * 100.0)));
+            }
+        }
     }
 
-    private void loadCourseManager() {
-        courseManager = new CourseManager();
-        this.courses = courseManager.getCourses();
-    }
-
-    private void loadStudentManager() {
+    private void loadStudentManager() { // Loads all students
         studentManager = new StudentManager();
         this.students = studentManager.getStudents();
+    }
+
+    private void loadModuleManager() { // Loads all modules
+        moduleManager = new ModuleManager();
+        this.modules = moduleManager.getModules();
     }
 
     public ArrayList<WatchPercentage> getWatchPercentages() {
