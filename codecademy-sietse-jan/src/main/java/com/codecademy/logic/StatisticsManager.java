63098e3307dfc5d8142c38e8d42c3a982e7cbeb2
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2024-11-23 22:35:00 +0100

    DummyData creation

--- codecademy-sietse-jan/src/main/java/com/codecademy/logic/StatisticsManager.java
+++ codecademy-sietse-jan/src/main/java/com/codecademy/logic/StatisticsManager.java
@@ -1,5 +1,21 @@
 package com.codecademy.logic;
 
+import java.util.ArrayList;
+
+import com.codecademy.domain.Webcast;
+
 public class StatisticsManager {
-    
+    private ArrayList<Webcast> top3Webcasts;
+
+    public StatisticsManager() {
+        createDummyData();
+    }
+
+    private void createDummyData() { // This method creates the temp. dummy data
+        top3Webcasts = new ArrayList<Webcast>();
+
+        top3Webcasts.add(new Webcast("NX workshop Guide", "Een video over hoe NX workshop werkt", "Sietse", "Microsoft", 678, "youtube.com/dkawoke", 67498));
+        top3Webcasts.add(new Webcast("Java Basics", "Introductie tot Java programmeren", "Jan", "Codecademy", 1234, "youtube.com/javabasics", 58342));
+        top3Webcasts.add(new Webcast("Advanced Python", "Geavanceerde technieken in Python", "Piet", "Apple", 5678, "youtube.com/advancedpython", 38742));
+    }
 }
