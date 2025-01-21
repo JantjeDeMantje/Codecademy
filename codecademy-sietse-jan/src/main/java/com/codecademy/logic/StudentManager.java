84bb2a7df3113077fd4874d86e20ffa3b5f5a408
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2025-01-21 20:28:00 +0100

    StudentManager update
    Added relation with the studentDAO
    
    Removed dummy data

--- codecademy-sietse-jan/src/main/java/com/codecademy/logic/StudentManager.java
+++ codecademy-sietse-jan/src/main/java/com/codecademy/logic/StudentManager.java
@@ -1,5 +1,6 @@
 package com.codecademy.logic;
 
+import com.codecademy.dataStorage.StudentDAO;
 import com.codecademy.domain.Student;
 
 import javafx.scene.control.TableView;
@@ -7,35 +8,25 @@ import javafx.scene.control.TableView;
 import java.util.ArrayList;
 
 public class StudentManager {
-
-    private ArrayList<Student> students;
+    private StudentDAO studentDAO;
 
     public StudentManager() {
-        createDummyData(); // Calls the createDummyData method when started.
-    }
-
-    private void createDummyData() { // This method creates the temp. dummy data
-        students = new ArrayList<Student>();
-
-        students.add(new Student("Sietse 't Hooft", "sietse@gmail.com", "01-01-2004", "Man", "1234AB", "Breda", "Nederland"));
-        students.add(new Student("Jan Roelofs", "jan@gmail.com", "01-01-2004", "Man", "4321AB", "Breda", "Nederland"));
-        students.add(new Student("Gert van Dijk", "gert@gmail.com", "01-01-2004", "Man", "3412AB", "Breda", "Nederland"));
-
+        studentDAO = new StudentDAO();
     }
 
     // This method returns the students arraylist
     public ArrayList<Student> getStudents() {
-        return students;
+        return studentDAO.getAllStudents();
     }
 
     // This method creates a new student object
     public Student createStudent(String name, String email, String birthdate, String gender, String zipcode,
             String city, String country, TableView<Student> studentTable) {
 
-        Student newStudent = new Student(name, email, birthdate, gender, zipcode, city, country);
+        // Student newStudent = new Student(name, email, birthdate, gender, zipcode, city, country);
 
-        studentTable.getItems().add(newStudent); // Adds the new student to the table
-        return newStudent;
+        // studentTable.getItems().add(newStudent); // Adds the new student to the table
+        return null;
     }
 
     // This method deletes a student object
