b826e046b3aa5a5f883df2ba032656b96cd64822
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2025-01-21 20:26:37 +0100

    Creation StudentDAO

---
+++ codecademy-sietse-jan/src/main/java/com/codecademy/dataStorage/StudentDAO.java
@@ -0,0 +1,60 @@
+package com.codecademy.dataStorage;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.sql.Date;
+
+import com.codecademy.domain.Student;
+
+public class StudentDAO{
+    private DatabaseConnection databaseConnection;
+
+    public StudentDAO(){
+        databaseConnection = new DatabaseConnection();
+    }
+
+     public ArrayList<Student> getAllStudents() {
+        System.out.println("Getting all students");
+        ArrayList<Student> students = new ArrayList<>();
+        Connection connection = DatabaseConnection.getConnection();
+        if (connection != null) {
+            try {
+                Statement statement = connection.createStatement();
+                String query = "SELECT StudentId, EmailAddress, Name, Birthday, Gender, Zipcode, City, Country\r\n" + //
+                                        "FROM Student\r\n" + //
+                                        "JOIN Address ON Student.AddressId = Address.AddressId";
+                ResultSet resultSet = statement.executeQuery(query);
+                while (resultSet.next()) {
+                    int studentId = resultSet.getInt("StudentId");
+                    String name = resultSet.getString("Name");
+                    String email = resultSet.getString("EmailAddress");
+                    Date birthdate = resultSet.getDate("Birthday");
+                    String gender = resultSet.getString("Gender");
+                    String zipcode = resultSet.getString("Zipcode");	
+                    String city = resultSet.getString("City");	
+                    String country = resultSet.getString("Country");
+                    students.add(new Student(studentId, name, email, birthdate, gender, zipcode, city, country));
+                    printResult(students);
+                }
+            } catch (SQLException e) {
+                System.out.println("Error: " + e.getMessage());
+            } finally {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    System.out.println("Error closing connection: " + e.getMessage());
+                }
+            }
+        }
+        return students;
+    }
+
+    public void printResult(ArrayList<Student> students){
+        for(Student student : students){
+            System.out.println(student.getName());
+        }
+    }
+}
